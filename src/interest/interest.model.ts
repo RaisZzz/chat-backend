36490670fb37eb786bf8cdfb3b67a47f5067d38e
RaisZzz <beats.tapping@gmail.com>
2024-08-04 05:41:07 +0500

    ADD: Interests icon name

--- src/interest/interest.model.ts
+++ src/interest/interest.model.ts
@@ -19,21 +19,28 @@ export class Interest extends Model<Interest, CreateInterestDto> {
   id: number;
 
   @ApiProperty({ example: 'Спорт', description: 'Название интереса' })
-  @Column({ type: DataType.STRING, unique: true, allowNull: false })
+  @Column({ type: DataType.TEXT, unique: true, allowNull: false })
   title: string;
 
   @ApiProperty({ example: 'Divorced', description: 'Название на английском' })
-  @Column({ type: DataType.STRING, unique: true, allowNull: false })
+  @Column({ type: DataType.TEXT, unique: true, allowNull: false })
   title_en: string;
 
   @ApiProperty({ example: 'Ajrashgan', description: 'Название на узбекском' })
-  @Column({ type: DataType.STRING, unique: true, allowNull: false })
+  @Column({ type: DataType.TEXT, unique: true, allowNull: false })
   title_uz: string;
 
   @ApiProperty({
     example: 'lorem',
     description: 'Название на Узбекистанском (кириллица)',
   })
-  @Column({ type: DataType.STRING, allowNull: false })
+  @Column({ type: DataType.TEXT, allowNull: false })
   title_uz_cyr: string;
+
+  @ApiProperty({
+    example: 'car',
+    description: 'Имя иконки',
+  })
+  @Column({ type: DataType.TEXT })
+  iconName: string;
 }
