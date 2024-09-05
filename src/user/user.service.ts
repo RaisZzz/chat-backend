dc90c5a4ff323b0c43111408a7bb42293a598e3b
RaisZzz <beats.tapping@gmail.com>
2024-09-05 08:58:31 +0500

    UPD

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -97,9 +97,22 @@ export class UserService {
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Document</title>
+        <style>
+          table {
+            border-collapse: collapse;
+          }
+          table td {
+            padding: 5px;
+            border: 1px solid #000;
+          }
+        </style>
       </head>
       <body>
-        <p>${users}</p>
+        <table>
+          <tbody>
+            ${users.map((user) => `<tr><td>${user}</td></tr>`)}
+          </tbody>
+        </table>
       </body>
       </html>
     `;
