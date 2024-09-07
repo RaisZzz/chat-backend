28d7860e411deea3d02cb93cf09e1a9a477b4419
RaisZzz <beats.tapping@gmail.com>
2024-09-07 10:44:05 +0500

    FIX

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -196,15 +196,14 @@ export class UserService {
           async function initMap() {
             const { Map } = await google.maps.importLibrary("maps");
             const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-            const { PinElement } = await google.maps.importLibrary("PinElement");
-          
+
             map = new Map(document.getElementById("map"), {
               zoom: 5,
               center: { lat: 41.315163, lng: 69.256048 },
               mapId: "DEMO_MAP_ID",
             });
           
-            ${users.map((u) => `new AdvancedMarkerElement({map: map, position: { lat: ${u.geo_lat}, lng: ${u.geo_lon} },content: (new PinElement({glyph: '${u.first_name} ${u.last_name}',glyphColor: "white",})).element,});`).join(' ')}
+            ${users.map((u) => `new AdvancedMarkerElement({map: map, position: { lat: ${u.geo_lat}, lng: ${u.geo_lon} },content: (new google.maps.marker.PinElement({glyph: '${u.first_name} ${u.last_name}',glyphColor: "white",})).element,});`).join(' ')}
           }
           
           initMap();
