9ce08f92e2e939597a6737abe6647af76f7c2bf9
RaisZzz <beats.tapping@gmail.com>
2024-10-11 15:53:51 +0500

    FIX

--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -4,11 +4,15 @@ import { CityService } from './city.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { City } from './city.model';
 import { AuthModule } from '../auth/auth.module';
+import { User } from '../user/user.model';
 
 @Module({
   controllers: [CityController],
   providers: [CityService],
-  imports: [SequelizeModule.forFeature([City]), forwardRef(() => AuthModule)],
+  imports: [
+    SequelizeModule.forFeature([City, User]),
+    forwardRef(() => AuthModule),
+  ],
   exports: [CityService],
 })
 export class CityModule {}
