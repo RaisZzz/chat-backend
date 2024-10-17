51c73bd217135529a43c22d80dbd8c4491db9334
RaisZzz <beats.tapping@gmail.com>
2024-10-17 15:35:11 +0500

    FIX

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -483,6 +483,7 @@ export class UserService {
         (select array(select image_id from user_verification_image where user_id = "user".id)) as "verificationImages"
         from "user"
         where id <> ${user.id}
+        ${getUsersDto.wearsHijab?.length && getUsersDto.wearsHijab.includes(true) ? 'and sex = 0' : ''}
         and ('admin' <> ANY(select value from role where id in (select role_id from user_role where user_id = "user".id)))
         ${filterQuery ? `and (${filterQuery})` : ''}
         ${verifiedQuery ? `and (${verifiedQuery})` : ''}
