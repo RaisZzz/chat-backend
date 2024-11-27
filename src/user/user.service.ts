1ce2d2a10aa52a130a8bb5896adbe1df85648849
RaisZzz <beats.tapping@gmail.com>
2024-11-27 10:07:55 +0500

    FIX

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -739,6 +739,7 @@ export class UserService {
 
   async getUserById(user: User, getDto: GetUserById): Promise<User> {
     return await this.userRepository.findOne({
+      include: { all: true },
       attributes: { exclude: excludedUserAttributes },
       where: { id: getDto.userId, sex: user.sex == 0 ? 1 : 0 },
     });
