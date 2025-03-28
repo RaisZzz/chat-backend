2877cbb9c29ca193fb468ca5b61927e9c665e979
RaisZzz <beats.tapping@gmail.com>
2025-03-28 09:21:31 +0500

    FIX

--- src/purchase/dto/delete-purchase.dto.ts
+++ src/purchase/dto/delete-purchase.dto.ts
@@ -1,6 +1,8 @@
 import { IsInt } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class DeletePurchaseDto {
+  @ApiProperty({ example: 1, description: 'Purchase ID' })
   @IsInt()
   readonly purchaseId: number;
 }
